c14165942c5a92ffa7730691da4d9ee5f10073a5
jagan10NS <ns.jagansenthilkumar@gmail.com>
2024-02-17 09:23:26 +0530

    imported Files

---
+++ NewTestFile1.py
@@ -0,0 +1,10 @@
+class NewTestFile1():
+    def StudentDetails(self,name,age):
+        self.Name=name
+        self.age=age
+    def display(self):
+        print("Name:",self.Name)
+        print("Age:",self.age)
+        print("This is NewTestFile1...")
+        
+
---
+++ NewTestFile2.py
@@ -0,0 +1,11 @@
+from NewTestFile1 import NewTestFile1
+
+object = NewTestFile1()
+
+object.StudentDetails("jagan",19)
+object.display()
+
+def NewTestFile2():
+    print("This is NewTestFile2...")
+
+NewTestFile2()

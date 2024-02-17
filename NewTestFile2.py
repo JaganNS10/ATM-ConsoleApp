6091fdcb97a8bca0eb4c09b850bda0f375bd4e28
jagan10NS <ns.jagansenthilkumar@gmail.com>
2024-02-17 08:17:43 +0530

    imported Files

---
+++ NewTestFile2.py
@@ -0,0 +1,10 @@
+from NewTestFile1 import Calculator
+
+if __name__ == "__main__":
+    objectforNewTestFile1 = Calculator()
+    objectforNewTestFile1.display()
+
+    def NewTestFile2():
+        print("This is File2...")
+        
+    NewTestFile2()

68e20ffd3aa4f83e1ac574ea32d0f77ad7e33665
jagan10NS <ns.jagansenthilkumar@gmail.com>
2024-02-17 08:52:41 +0530

    Imported Files

---
+++ Main.py
@@ -0,0 +1,9 @@
+from Calculator import Calculator
+
+if __name__ == "__main__":
+    objectforCalculator = Calculator()
+    objectforCalculator.display()
+
+    def Main():
+        print("This Is File2...")
+    Main()
